890498237eead7c9953f23d1214b4ac1ff8dacfd
sasha5434 <sasha5434@inbox.ru>
2020-02-03 21:32:39 +0300

    version 1.5 hotfix

--- public/index.php
+++ public/index.php
@@ -12,6 +12,7 @@ header('Access-Control-Allow-Origin: *');
 define ( 'DONTHACKME', true );
 define ( 'ROOT_DIR', dirname ( __FILE__ ) );
 define ( 'CORE_DIR', ROOT_DIR . '/core' );
+define ( 'LANG_DIR', ROOT_DIR . '/lang' );
 
 require_once ROOT_DIR . '/core/core.php';
 
